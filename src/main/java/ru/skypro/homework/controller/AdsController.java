adcce2fdcd1ba15bdc56e0dac7a11ca4fa8e226b
tinkov <vasy499@mail.ru>
2023-06-27 18:40:23 +0300

    AdsController & AdsDto commit

---
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -0,0 +1,52 @@
+package ru.skypro.homework.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ads")
+@CrossOrigin(value = "http://localhost:3000")
+public class AdsController {
+    @GetMapping
+    public ResponseEntity<?> getAdsAll() {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addAd() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAd() {
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAd() {
+            return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getAdsMe() {
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateAd() {
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping(value = "/{id}/image",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<?> updateImage() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/title/{titlePart}")
+    public ResponseEntity<?> getAdsByNamePart() {
+        return ResponseEntity.ok().build();
+    }
+}
