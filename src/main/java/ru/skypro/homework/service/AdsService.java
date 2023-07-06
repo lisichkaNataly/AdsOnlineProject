326a0b1b67a99573f6cf6d736f4326a3e9be903b
tinkov <vasy499@mail.ru>
2023-07-06 17:01:12 +0300

    added adsMapper & AdsService & ImageService

--- src/main/java/ru/skypro/homework/service/AdsService.java
+++ src/main/java/ru/skypro/homework/service/AdsService.java
@@ -3,12 +3,18 @@ package ru.skypro.homework.service;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.*;
 import ru.skypro.homework.entity.Ads;
+import ru.skypro.homework.entity.Image;
+import ru.skypro.homework.entity.User;
 import ru.skypro.homework.mapper.AdsListMapper;
 import ru.skypro.homework.mapper.AdsMapper;
 import ru.skypro.homework.repository.AdsRepository;
 import ru.skypro.homework.repository.CommentRepository;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -16,12 +22,23 @@ import java.util.Optional;
 @AllArgsConstructor
 public class AdsService {
 
+    private final AdsRepository adsRepository;
+    private final AdsMapper mapper;
+    private final UserService userService;
+    private final ImageService imageService;
+    private final CommentRepository commentRepository;
+
     public Ads getAdById(Integer id) {
-        return null;
+        return adsRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Ad with id=" + id + " not found!"));
     }
 
-    public AdsDto createAd() {
-        return null;
+    public AdsDto createAd(CreateAdsDto createAdsDto, MultipartFile imageFile, String username) throws IOException {
+        Ads ad = mapper.toAds(createAdsDto);
+        User author = userService.getUserByUsername(username);
+        ad.setAuthor(author);
+        ad.setImage(imageService.uploadImage(imageFile));
+        return mapper.toDto(adsRepository.save(ad));
     }
 
     public ResponseWrapperAdsDto getAdsAll() {
@@ -32,8 +49,18 @@ public class AdsService {
         return null;
     }
 
-    public boolean deleteAd(Integer id, String username)  {
-        return false;
+    public boolean deleteAd(Integer id, String username) throws IOException {
+        User user = userService.getUserByUsername(username);
+        Ads ad = getAdById(id);
+        if (user.equals(ad.getAuthor()) || user.getRole() == Role.ADMIN) {
+            Image image = ad.getImage();
+            commentRepository.deleteAll(ad.getCommentsList());
+            adsRepository.delete(ad);
+            imageService.deleteImage(image);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public Optional<AdsDto> updateAd(Integer id, String username) {
--- src/main/java/ru/skypro/homework/service/ImageService.java
+++ src/main/java/ru/skypro/homework/service/ImageService.java
@@ -1,19 +1,47 @@
 package ru.skypro.homework.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.entity.Image;
+import ru.skypro.homework.repository.ImageRepository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.UUID;
 
 @Service
 public class ImageService {
-
-    public Image uploadImage(MultipartFile imageFile) {
-        Image image = new Image();
-
-        return null;
+    private final ImageRepository imageRepository;
+    public ImageService(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
     }
 
-    public void deleteImage(Image image) {
+    @Value("${path.to.image.folder}")
+    private String imageDir;
 
+    public Image uploadImage(MultipartFile imageFile) throws IOException {
+
+        Image image = new Image();
+        Path filePath = Path.of(imageDir, UUID.randomUUID().toString());
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        Files.write(filePath, imageFile.getBytes());
+
+        image.setFilePath(filePath.toString());
+        image.setFileSize(imageFile.getSize());
+        image.setMediaType(imageFile.getContentType());
+
+        return imageRepository.save(image);
+    }
+
+    public void deleteImage(Image image) throws IOException {
+
+        if (image != null) {
+            Path filePath = Path.of(image.getFilePath());
+            Files.deleteIfExists(filePath);
+            imageRepository.delete(image);
+        }
     }
 }
