d856c0960b9e4af68500b80b4c97b92946b3d62c
natalamoiseeva <natalyatf@yandex.ru>
2023-07-09 21:11:33 +0500

    добавила в AdsListMapper и в AdsListMapperImpl

--- src/main/java/ru/skypro/homework/mapper/AdsListMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdsListMapper.java
@@ -2,12 +2,21 @@ package ru.skypro.homework.mapper;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import ru.skypro.homework.dto.AdsDto;
+import ru.skypro.homework.entity.Ads;
+
+import java.util.List;
 
 
 @Mapper(componentModel = "spring",
         uses = {AdsMapper.class},
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AdsListMapper {
+
+    List<AdsDto> toDto(List <Ads> adsList);
+
+
+
 
 
 }
--- src/main/java/ru/skypro/homework/mapper/CommentMapper.java
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -2,10 +2,44 @@ package ru.skypro.homework.mapper;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import ru.skypro.homework.dto.CommentDto;
+import ru.skypro.homework.entity.Comment;
+import ru.skypro.homework.entity.Image;
+import ru.skypro.homework.entity.User;
+
+import java.util.Objects;
 
 
-@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface CommentMapper {
+
+    default CommentDto toDto(Comment comment) {
+
+        if (comment == null) {
+            return null;
+        }
+
+        CommentDto commentDto = new CommentDto();
+        commentDto.setPk(comment.getCommentId());
+        commentDto.setText(comment.getText());
+
+        commentDto.setCreatedAt(Objects.requireNonNullElse(comment.getCreatedAt(), 0L));
+
+        User author = comment.getAuthor();
+        if (author != null) {
+
+            commentDto.setAuthor(comment.getAuthor().getId());
+            commentDto.setAuthorFirstName(comment.getAuthor().getFirstName());
+
+            Image image = author.getImage();
+            if (image != null) {
+                commentDto.setAuthorImage("/image/" + comment.getAuthor().getImage().getId());
+            }
+        }
+
+        return commentDto;
+    }
 
 
 
