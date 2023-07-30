6897e8a3399a7876ba345b6db90be92ff5fac56f
natalamoiseeva <natalyatf@yandex.ru>
2023-07-30 16:54:21 +0500

    корректировки

--- src/main/java/ru/skypro/homework/service/AdsService.java
+++ src/main/java/ru/skypro/homework/service/AdsService.java
@@ -18,12 +18,9 @@ import java.util.Collection;
 public interface AdsService {
 
     Collection<Ads> getAllAds();
-
     Collection<Ads> getAdsMe(Authentication authentication);
     Ads getAdsById(Long adId);
-
     Ads addAds(CreateAdsDto createAdsDto, MultipartFile imageFiles, Authentication authentication);
-
     Ads removeAdsById(Long adId, Authentication authentication);
     Comment getComment(long adPk, long id);
     Collection<Comment> getComments(long adPk);
@@ -32,9 +29,7 @@ public interface AdsService {
     Ads updateAds(Long adId, CreateAdsDto createAdsDto, Authentication authentication);
     Comment updateComments(long adPk, long id, Comment comment, Authentication authentication);
     void updateAdsImage(long id, MultipartFile image, Authentication authentication);
-    /**
-     * Получение из базы данных объявления с указанным id.
-     */
+
 
 
 }
